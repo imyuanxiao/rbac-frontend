d5ce3b213f8e9817465df7e28ab553f045792a07
imyuanxiao <imyuanxiao@126.com>
2023-05-13 21:41:49 +0100

    优化EditUser组件，用表单进行数据提交和验证

--- src/pages/user/DeleteUser.tsx
+++ src/pages/user/DeleteUser.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Modal} from 'antd';
 import {UserPageVO} from "../../api/types";
 import {deleteUser} from "../../api/api";
@@ -11,13 +11,19 @@ function DeleteUser({ isBatchDelete, userIds, user, modalOpen, setModalOpen, onU
     setModalOpen: (open: boolean) => void;
     onUpdate: () => void }) {
 
+    const [confirmLoading, setConfirmLoading] = useState<boolean>(false);
+
     const deleteConfirmationText = isBatchDelete
         ? '请确认是否批量删除用户？'
         : `请确认是否删除用户: ${user.username}？`;
 
     const handleOk = async () => {
+        setConfirmLoading(true)
         if (await deleteUser(isBatchDelete? userIds : [user.id])) {
+            setConfirmLoading(false)
             onUpdate();
+        }else{
+            setConfirmLoading(false)
         }
     };
 
@@ -32,6 +38,7 @@ function DeleteUser({ isBatchDelete, userIds, user, modalOpen, setModalOpen, onU
             cancelText="取消"
             okText="确认"
             destroyOnClose={true}
+            confirmLoading={confirmLoading}
         >
             <div>{deleteConfirmationText}</div>
             <div>注意：删除后无法恢复！</div>
--- src/pages/user/EditUser.tsx
+++ src/pages/user/EditUser.tsx
@@ -1,90 +1,103 @@
 import React, {useEffect, useState} from 'react';
-import { UserOutlined } from '@ant-design/icons';
-import {Modal, Input, Select, message} from 'antd';
+import { Modal, Form, Input, Select, message } from 'antd';
 import { getRoleOptions } from '../../utils/AttayUtil';
-import {Option, User, UserPageVO} from "../../api/types";
-import {addUser, updateUser} from "../../api/api";
-import {isValidUsername} from "../../utils/ValidUtil";
+import { Option, UserPageVO } from "../../api/types";
+import { addUser, updateUser } from "../../api/api";
 
-/**
- * 将本地存储的角色信息，转换成为多选框选项
- */
-const options: Option[] = getRoleOptions()
+const options: Option[] = getRoleOptions();
 
 function EditUser({ isEdit, user, modalOpen, setModalOpen, onUpdate }: {
     isEdit: boolean;
     user: UserPageVO;
     modalOpen: boolean;
     setModalOpen: (open: boolean) => void;
-    onUpdate: () => void }) {
-    
-    // 保存选中的角色信息
-    const [selectedRoleIds, setSelectedRoleIds] = useState<number[]>([]);
-    // 保存input框中的内容
-    const [username, setUsername] = useState<string>('');
+    onUpdate: () => void;
+}) {
 
-    const handleRoleIdsChange = (value: any) => {
-        setSelectedRoleIds([].concat(...value));
+    // 用于操作表单
+    const [form] = Form.useForm();
+    // 等待响应数据返回期间按钮会显示loading动画
+    const [confirmLoading, setConfirmLoading] = useState<boolean>(false);
+
+    /**
+     * 提交表单
+     */
+    const handleOk = () => {
+        // 表单加载中
+        setConfirmLoading(true);
+        // 验证表单数据有效性
+        form.validateFields().then(async (values) => {
+                console.log(values);
+                if ((isEdit && await updateUser(values)) || (await addUser(values))) {
+                    onUpdate();
+                }
+            }
+        ).catch((error) => {
+            message.error('Form validation failed.');
+        }).finally(() => {
+            setConfirmLoading(false)
+        })
     };
 
-    const handleOk = async () => {
-        if (isEdit) {
-            if(await updateUser({
-                id: user.id,
-                username: username as string,
-                roleIds: selectedRoleIds as number[]
-            } as User)){
-                onUpdate();
-            }
-        } else {
-            // 验证username属性
-            if (!isValidUsername(username)) {
-                message.error('Username must be between 4 and 12 characters in length.');
-                return;
-            }
-            if (await addUser(
-                {username: username as string, roleIds: selectedRoleIds as number[]
-                })) {
-                onUpdate();
-            }
-        }
-    };
-
+    /**
+     * 初始化表单数据
+     */
     useEffect(() => {
-        setUsername(user.username);
-        setSelectedRoleIds(user.roleIds);
-    }, [user.username]);
+        if (isEdit) {
+            form.setFieldsValue({
+                id: user.id,
+                username: user.username,
+                roleIds: user.roleIds.map(String)
+            });
+        }else{
+            form.resetFields();
+        }
+    }, [modalOpen, user, isEdit, form]);
 
     return (
         <Modal
-            title={isEdit? "编辑数据" : "新增用户"}
+            title={isEdit ? "编辑数据" : "新增用户"}
             open={modalOpen}
             onCancel={() => {
                 setModalOpen(false);
             }}
             onOk={handleOk}
-            cancelText="取消"
-            okText="确认"
             destroyOnClose={true}
+            confirmLoading={confirmLoading}
         >
+            <Form
+                form={form}
+            >
+                <Form.Item
+                    label="userId"
+                    name="id"
+                    style={{ display: 'none' }}
+                    rules={[
+                        { required: isEdit, message: 'Please input your username!' },
+                    ]}
+                >
+                </Form.Item>
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                        { required: true, message: 'Please input your username!' },
+                        { min: 4, max: 12, message: 'Length should be between 4 and 12 characters!' }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-            <p><b>Username</b></p>
-
-            <Input
-                value={username}
-                style={{ width: '100%' }}
-                prefix={<UserOutlined />}
-                onChange={(e)=>setUsername(e.target.value)}
-            />
-
-            <p><b>Role</b></p>
-            <Select
-                mode="tags"
-                defaultValue={user.roleIds.map(String)}
-                onChange={handleRoleIdsChange}
-                style={{ width: '100%' }}
-                options={options}
-            />
+                <Form.Item
+                    label="Role"
+                    name="roleIds"
+                >
+                    <Select
+                        mode="multiple"
+                        options={options}
+                    />
+                </Form.Item>
+            </Form>
         </Modal>
     );
 }
